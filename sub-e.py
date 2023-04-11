2ab18068c853a75d83700454aebe0f57590453b6
Sean Victor Hum <sean.hum@utoronto.ca>
2023-04-11 15:51:25 -0400

    Correct IP address problem with power supply

--- sub-e.py
+++ sub-e.py
@@ -28,10 +28,10 @@ school_ip = True
 #school_ip = False
 if (school_ip):
     scope = rm.open_resource('TCPIP0::192.168.0.253::hislip0::INSTR')
-    fxngen = rm.open_resource('TCPIP0::192.168.0.254::5025::SOCKET')
+    supply = rm.open_resource('TCPIP0::192.168.0.251::5025::SOCKET')
 else:
     scope = rm.open_resource('TCPIP0::192.168.2.253::hislip0::INSTR')
-    fxngen = rm.open_resource('TCPIP0::192.168.2.254::5025::SOCKET')
+    supply = rm.open_resource('TCPIP0::192.168.2.251::5025::SOCKET')
 
 # Define string terminations and timeouts
 scope.write_termination = '\n'
