fea949a2b678ae1ca69f576cdcdcee81c2cc0b55
Sean Victor Hum <sean.hum@utoronto.ca>
2025-03-21 12:07:23 -0400

    Update SubF to differential drive

--- sub-f.py
+++ sub-f.py
@@ -17,25 +17,31 @@ def user_prompt():
     str = input('Hit Enter to proceed or ! to abort:')
     if (str == '!'):
         print('Measurement aborted')
-        scope.write(':WGEN:OUTP OFF')
+        fxngen.write('OUTPut1 OFF')
+        fxngen.write('OUTPut2 OFF')
         scope.close()
         supply.close()
+        fxngen.close()
         sys.exit(1)
 
 # Open instrument connection(s)
 rm = pyvisa.ResourceManager()
-school_ip = True
-#school_ip = False
+#school_ip = True
+school_ip = False
 if (school_ip):
     scope = rm.open_resource('TCPIP0::192.168.0.253::hislip0::INSTR')
     supply = rm.open_resource('TCPIP0::192.168.0.251::5025::SOCKET')
+    fxngen = rm.open_resource('TCPIP0::192.168.0.254::5025::SOCKET')
 else:
     scope = rm.open_resource('TCPIP0::192.168.2.253::hislip0::INSTR')
     supply = rm.open_resource('TCPIP0::192.168.2.251::5025::SOCKET')
+    fxngen = rm.open_resource('TCPIP0::192.168.2.254::5025::SOCKET')
 
 # Define string terminations and timeouts
 scope.write_termination = '\n'
 scope.read_termination = '\n'
+fxngen.write_termination = '\n'
+fxngen.read_termination = '\n'
 supply.write_termination = '\n'
 supply.read_termination = '\n'
 scope.timeout = 10000           # 10s
@@ -43,9 +49,13 @@ supply.timeout = 10000          # 10s
 
 # Get ID info
 scope_id = scope.query('*IDN?').strip().split(',')
+fxngen_id = fxngen.query('*IDN?').strip().split(',')
 supply_id = supply.query('*IDN?').strip().split(',')
 print('Connected to oscilloscope:', scope_id[1], flush=True)
+print('Connected to function generator:', fxngen_id[1], flush=True)
 print('Connected to power supply:', supply_id[1], flush=True)
+
+## SCOPE
 
 # Set probe scaling to 1:1
 scope.write('CHANnel1:PROBe +1.0')
@@ -59,27 +69,54 @@ scope.write(':TRIG:EDGE:LEVel +0.0')
 #print('Trigger:', scope.query(':TRIG?'), flush=True)
 
 # Disable power supply output and wavegen output
-supply.write('OUTP OFF, (@2)')
-scope.write(':WGEN:OUTP OFF')
+#supply.write('OUTP OFF, (@2)')
+#scope.write(':WGEN:OUTP OFF')
+
+## FUNCTION GENERATOR
+
+print('\nNOTE: Excitation signal amplitude can be changed by modifying the')
+print('drive_amplitude variable in sub-f.py.')
+print()
+
+drive_amplitude = 1.0          # Set to input drive amplitude required (Vpp)
+
+# Set waveform generator output impedance to high Z
+fxngen.write('OUTPUT1:LOAD INF')
+fxngen.write('OUTPUT2:LOAD INF')
+fxngen.write('UNIT:ANGL DEG')
+
+# Setup waveform generator
+fxngen.write('SOUR1:FUNCtion SIN')
+fxngen.write('SOUR1:FREQuency %e' % (14e6))
+fxngen.write('SOUR1:VOLTage %e' % (drive_amplitude))
+fxngen.write('SOUR1:VOLTage:OFFSet +0.0')
+fxngen.write('SOUR1:PHASe:SYNC')
+fxngen.write('SOUR1:PHASe +0.0')
+#fxngen.write('OUTPut1 ON')
+
+fxngen.write('SOUR2:FUNCtion SIN')
+fxngen.write('SOUR2:FREQuency %e' % (14e6))
+fxngen.write('SOUR2:VOLTage %e' % (drive_amplitude))
+fxngen.write('SOUR2:VOLTage:OFFSet +0.0')
+fxngen.write('SOUR2:PHASe:SYNC')
+fxngen.write('OUTPut2:POL INV')
+#fxngen.write('OUTPut2 ON')
 
 print('Connect your subsystem as shown in the wiring diagram and power it on.')
 print('Make sure you have asserted the /TXEN line (set it low)!')
 user_prompt()
 
-# Turn on power supply and set up wavegen
-supply.write('OUTP ON, (@2)')
-scope.write(':WGEN:FUNC SIN')
-scope.write(':WGEN:FREQ 1.400E+07')
+# Turn on power supply (not necessary)
+#supply.write('OUTP ON, (@2)')
 
 # Measure idle current
 V = float(supply.query('VOLT? (@2)'))
 Iidle = float(supply.query('MEAS:CURR? CH2'))
 Pidle = V*Iidle
 
-# Set wavegen amplitude and enable
-drive_amplitude = 1.0          # Set to input drive amplitude required
-scope.write(':WGEN:volt %e' % (drive_amplitude))
-scope.write(':WGEN:OUTP ON')
+# Enable waveform generator
+fxngen.write('OUTPut1 ON')
+fxngen.write('OUTPut2 ON')
 
 # Setup acquisition
 scope.write(':TIMebase:SCAL +5.0E-08') # 50 ns/div
@@ -122,6 +159,7 @@ scope.write(':MARKer:X2Y2source FFT')
 scope.write(':MARK:MODE WAV')
 
 f0 = float(scope.query('WGEN:FREQ?'))
+f0 = float(fxngen.query('SOUR1:FREQ?'))
 print('Source frequency set to:', f0/1e6, 'MHz')
 
 # Measure harmonics
@@ -179,17 +217,24 @@ Vout = zeros(N, float)
 
 print('Measuring frequency response...')
 for k in range(N):
-    scope.write(':WGEN:FREQ %e' % freq[k])
+    fxngen.write('SOUR1:FREQuency %e' % (freq[k]))
+    fxngen.write('SOUR1:PHASe:SYNC')
+    fxngen.write('SOUR1:PHASe +0.0')
+    fxngen.write('SOUR2:FREQuency %e' % (freq[k]))
+    fxngen.write('SOUR2:PHASe:SYNC')
+    fxngen.write('OUTPut2:POL INV')
     time.sleep(1)
     Vout[k] = float(scope.query(':MEAS:VRMS? CHAN1'))
-    #print(freq[k], Vout[k])
+    print('Frequency = %f MHz, V = %f Vrms' % (freq[k]/1e6, Vout[k]))
 print('Done')
     
 # Turn of waveform generator and close connections
-scope.write(':WGEN:OUTP OFF')
+fxngen.write('OUTPut1 OFF')
+fxngen.write('OUTPut2 OFF')
 
 scope.close()
 supply.close()
+fxngen.close()
 
 # Save and plot data
 Prf = Vout**2/50
@@ -219,7 +264,8 @@ savefig('pout.png')
 
 # Plot output spectrum
 fig, ax = subplots()
-ax.stem(n, Pcoeffs, use_line_collection=True)
+#ax.stem(n, Pcoeffs, use_line_collection=True)
+ax.stem(n, Pcoeffs)
 ax.set_ylabel('RF output power [W]')
 ax.set_yscale('log')
 ax.grid(True)
